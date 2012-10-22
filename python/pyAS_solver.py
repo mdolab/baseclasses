545f5aca3d1c867eb1a60595e4229fd422b409fe
kenway <5203633+gkenway@users.noreply.github.com>
2012-10-22 16:57:40 -0400

    replaced all tabs with spaces

--- python/pyAS_solver.py
+++ python/pyAS_solver.py
@@ -15,14 +15,14 @@ Developers:
 
 History
 -------
-	v. 1.0	- Initial Class Creation (CAM, 2012)
+        v. 1.0  - Initial Class Creation (CAM, 2012)
 '''
 
 __version__ = '$Revision: $'
 
 '''
 ToDo:
-	- 
+        - 
 '''
 
 # =============================================================================
@@ -51,316 +51,316 @@ import pdb
 # AeroSolver Class
 # =============================================================================
 class ASSolver(object):
-	
-	'''
-	Abstract Class for Structural Solver Object
-	'''
-	
-	def __init__(self, name, category=None, def_options=None, informs=None, *args, **kwargs):
-		
-		'''
-		StructSolver Class Initialization
-		
-		Documentation last updated:  
-		'''
-		
-		# 
-		self.name = name
-		self.category = category
-		self.options = {}
-		self.options['defaults'] = def_options
-		self.informs = informs
-		
-		# Initialize Options
-		def_keys = def_options.keys()
-		for key in def_keys:
-			self.options[key] = def_options[key]
-		#end
-		koptions = kwargs.pop('options',{})
-		kopt_keys = koptions.keys()
-		for key in kopt_keys:
-			self.setOption(key,koptions[key])
-		#end
-		
-		
-	def __solve__(self, *args, **kwargs):
-		
-		'''
-		Run Analyzer (Analyzer Specific Routine)
-		
-		Documentation last updated: 
-		'''
-		
-		pass
-		
-		
-	def __call__(self,  *args, **kwargs):
-		
-		'''
-		Run Analyzer (Calling Routine)
-		
-		Documentation last updated: 
-		'''
-		
-		
-		# Checks
-		
-		# Solve Analysis Problem
-		self.__solve__(*args, **kwargs)
-		
-		return 
+    
+    '''
+    Abstract Class for Structural Solver Object
+    '''
+    
+    def __init__(self, name, category=None, def_options=None, informs=None, *args, **kwargs):
+        
+        '''
+        StructSolver Class Initialization
+        
+        Documentation last updated:  
+        '''
+        
+        # 
+        self.name = name
+        self.category = category
+        self.options = {}
+        self.options['defaults'] = def_options
+        self.informs = informs
+        
+        # Initialize Options
+        def_keys = def_options.keys()
+        for key in def_keys:
+            self.options[key] = def_options[key]
+        #end
+        koptions = kwargs.pop('options',{})
+        kopt_keys = koptions.keys()
+        for key in kopt_keys:
+            self.setOption(key,koptions[key])
+        #end
+        
+        
+    def __solve__(self, *args, **kwargs):
+        
+        '''
+        Run Analyzer (Analyzer Specific Routine)
+        
+        Documentation last updated: 
+        '''
+        
+        pass
+        
+        
+    def __call__(self,  *args, **kwargs):
+        
+        '''
+        Run Analyzer (Calling Routine)
+        
+        Documentation last updated: 
+        '''
+        
+        
+        # Checks
+        
+        # Solve Analysis Problem
+        self.__solve__(*args, **kwargs)
+        
+        return 
 
 
 
-	def getCoordinates(self):
-		'''
-		Return the set of coordinates for the
-		mesh
-		'''
-		
-		pass
+    def getCoordinates(self):
+        '''
+        Return the set of coordinates for the
+        mesh
+        '''
+        
+        pass
 
 
-	def setCoordinates(self,coordinates):
-		'''
-		Set the set of coordinates for the
-		mesh
-		'''
-		
-		pass
+    def setCoordinates(self,coordinates):
+        '''
+        Set the set of coordinates for the
+        mesh
+        '''
+        
+        pass
 
 
 
-	def totalDerivative(self,objective):
-		'''
-		Return the total derivative of the objective at surface
-		coordinates
-		'''
+    def totalDerivative(self,objective):
+        '''
+        Return the total derivative of the objective at surface
+        coordinates
+        '''
 
-		pass
-
-
-	def getStateSize(self):
-		'''
-		Return the number of degrees of freedom (states) that are
-		on this processor
-		'''
-
-		pass
+        pass
 
 
-	def getStates(self):
-		'''
-		Return the states on this processor.
-		'''
+    def getStateSize(self):
+        '''
+        Return the number of degrees of freedom (states) that are
+        on this processor
+        '''
 
-		pass
-
-
-	def setStates(self,states):
-		''' Set the states on this processor.'''
-
-		pass
-
-	def getResidual(self):
-		'''
-		Return the reisudals on this processor.
-		'''
-
-		pass
+        pass
 
 
-	def getSolution(self):
-		'''
-		Retrieve the solution dictionary from the solver
-		'''
-		
-		pass
+    def getStates(self):
+        '''
+        Return the states on this processor.
+        '''
+
+        pass
 
 
-	def initAdjoint(self, *args, **kwargs):
-		'''
-		Initialize the Ajoint problem for this test case
-		'''
-		pass
+    def setStates(self,states):
+        ''' Set the states on this processor.'''
+
+        pass
+
+    def getResidual(self):
+        '''
+        Return the reisudals on this processor.
+        '''
+
+        pass
 
 
-	def solveAdjoint(self,objective, *args, **kwargs):
-		'''
-		Solve the adjoint problem for the desired objective functions.
+    def getSolution(self):
+        '''
+        Retrieve the solution dictionary from the solver
+        '''
+        
+        pass
 
-		objectives - List of objective functions
-	
-		'''
-	
-		self._on_adjoint(objective,*args,**kwargs)
-			
-		
-	def _on_adjoint(self, objective, *args, **kwargs):
-		
-		'''
-		Adjoint
-		
-		Documentation last updated:  May. 26, 2008 - Ruben E. Perez
-		'''	
-		
-		# 
-		pass
-		
 
-	def _on_setOption(self, name, value):
-		
-		'''
-		Set Optimizer Option Value (Optimizer Specific Routine)
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		raise NotImplementedError()
-		
+    def initAdjoint(self, *args, **kwargs):
+        '''
+        Initialize the Ajoint problem for this test case
+        '''
+        pass
 
-	def setOption(self, name, value):
-		
-		'''
-		Set Optimizer Option Value (Calling Routine)
-		
-		Keyword arguments:
-		-----------------
-		name -> STRING: Option Name
-		value -> SCALAR or BOOLEAN: Option Value
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		# 
-		def_options = self.options['defaults']
-		if def_options.has_key(name):
-			if (type(value) == def_options[name][0]):
-				self.options[name] = [type(value),value]
-			else:
-				raise IOError('Incorrect ' + repr(name) + ' value type')
-			#end
-		else:
-			print '%s is not a valid option name'%(name)
-			raise InputError('Not a valid option name')
-		#end
-		
-		# 
-	
-		self._on_setOption(name, value)
-		
-	def _on_getOption(self, name):
-		
-		'''
-		Get Optimizer Option Value (Optimizer Specific Routine)
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		raise NotImplementedError()
-		
-	def getOption(self, name):
-		
-		'''
-		Get Optimizer Option Value (Calling Routine)
-		
-		Keyword arguments:
-		-----------------
-		name -> STRING: Option Name
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		# 
-		def_options = self.options['defaults']
-		if def_options.has_key(name):
-			return self.options[name][1]
-		else:	
-			raise InputError(repr(name) + ' is not a valid option name')
-		#end
-		
-		# 
-		self._on_getOption(name)
-		
-	def _on_getInform(self, info):
-		
-		'''
-		Get Optimizer Result Information (Optimizer Specific Routine)
-		
-		Keyword arguments:
-		-----------------
-		id -> STRING: Option Name
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		raise NotImplementedError()
-		
-		
-	def getInform(self, infocode={}):
-		
-		'''
-		Get Optimizer Result Information (Calling Routine)
-		
-		Keyword arguments:
-		-----------------
-		name -> STRING: Option Name
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		# 
-		if (infocode == {}):
-			return self.informs
-		else:
-			return self._on_getInform(infocode)
-		#end
-		
-		
-	def ListAttributes(self):
-		
-		'''
-		Print Structured Attributes List
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		ListAttributes(self)
-	
+
+    def solveAdjoint(self,objective, *args, **kwargs):
+        '''
+        Solve the adjoint problem for the desired objective functions.
+
+        objectives - List of objective functions
+    
+        '''
+    
+        self._on_adjoint(objective,*args,**kwargs)
+            
+        
+    def _on_adjoint(self, objective, *args, **kwargs):
+        
+        '''
+        Adjoint
+        
+        Documentation last updated:  May. 26, 2008 - Ruben E. Perez
+        '''     
+        
+        # 
+        pass
+        
+
+    def _on_setOption(self, name, value):
+        
+        '''
+        Set Optimizer Option Value (Optimizer Specific Routine)
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        raise NotImplementedError()
+        
+
+    def setOption(self, name, value):
+        
+        '''
+        Set Optimizer Option Value (Calling Routine)
+        
+        Keyword arguments:
+        -----------------
+        name -> STRING: Option Name
+        value -> SCALAR or BOOLEAN: Option Value
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        # 
+        def_options = self.options['defaults']
+        if def_options.has_key(name):
+            if (type(value) == def_options[name][0]):
+                self.options[name] = [type(value),value]
+            else:
+                raise IOError('Incorrect ' + repr(name) + ' value type')
+            #end
+        else:
+            print '%s is not a valid option name'%(name)
+            raise InputError('Not a valid option name')
+        #end
+        
+        # 
+    
+        self._on_setOption(name, value)
+        
+    def _on_getOption(self, name):
+        
+        '''
+        Get Optimizer Option Value (Optimizer Specific Routine)
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        raise NotImplementedError()
+        
+    def getOption(self, name):
+        
+        '''
+        Get Optimizer Option Value (Calling Routine)
+        
+        Keyword arguments:
+        -----------------
+        name -> STRING: Option Name
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        # 
+        def_options = self.options['defaults']
+        if def_options.has_key(name):
+            return self.options[name][1]
+        else:   
+            raise InputError(repr(name) + ' is not a valid option name')
+        #end
+        
+        # 
+        self._on_getOption(name)
+        
+    def _on_getInform(self, info):
+        
+        '''
+        Get Optimizer Result Information (Optimizer Specific Routine)
+        
+        Keyword arguments:
+        -----------------
+        id -> STRING: Option Name
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        raise NotImplementedError()
+        
+        
+    def getInform(self, infocode={}):
+        
+        '''
+        Get Optimizer Result Information (Calling Routine)
+        
+        Keyword arguments:
+        -----------------
+        name -> STRING: Option Name
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        # 
+        if (infocode == {}):
+            return self.informs
+        else:
+            return self._on_getInform(infocode)
+        #end
+        
+        
+    def ListAttributes(self):
+        
+        '''
+        Print Structured Attributes List
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        ListAttributes(self)
+    
 
 
 #==============================================================================
 # 
 #==============================================================================
 def ListAttributes(self):
-		
-		'''
-		Print Structured Attributes List
-		
-		Documentation last updated:  March. 24, 2008 - Ruben E. Perez
-		'''
-		
-		print '\n'
-		print 'Attributes List of: ' + repr(self.__dict__['name']) + ' - ' + self.__class__.__name__ + ' Instance\n'
-		self_keys = self.__dict__.keys()
-		self_keys.sort()
-		for key in self_keys:
-			if key != 'name':
-				print str(key) + ' : ' + repr(self.__dict__[key])
-			#end
-		#end
-		print '\n'
-	
+        
+        '''
+        Print Structured Attributes List
+        
+        Documentation last updated:  March. 24, 2008 - Ruben E. Perez
+        '''
+        
+        print '\n'
+        print 'Attributes List of: ' + repr(self.__dict__['name']) + ' - ' + self.__class__.__name__ + ' Instance\n'
+        self_keys = self.__dict__.keys()
+        self_keys.sort()
+        for key in self_keys:
+            if key != 'name':
+                print str(key) + ' : ' + repr(self.__dict__[key])
+            #end
+        #end
+        print '\n'
+    
 
 
 #==============================================================================
 # Optimizer Test
 #==============================================================================
 if __name__ == '__main__':
-	
-	print 'Testing ...'
-	
-	# Test Optimizer
-	azr = ASSolver('Test')
-	azr.ListAttributes()
-	
+    
+    print 'Testing ...'
+    
+    # Test Optimizer
+    azr = ASSolver('Test')
+    azr.ListAttributes()
+    
--- python/pyAero_flow.py
+++ python/pyAero_flow.py
@@ -68,104 +68,104 @@ class Flow(object):
 		Keyword Arguments:
 		------------------
 		name -> STRING: Flow Instance Name
-		mach -> SCALAR: Mach Number
-		alpha -> SCALAR: Angle of Attack
-		beta -> SCALAR: Sideslip Angle
-		phat -> SCALAR: Non-Dimensional Roll Rate
-		rhat -> SCALAR: Non-Dimensional Yaw Rate
-		qhat -> SCALAR: Non-Dimensional Pitch Rate
-		
-		Attributes:
-		-----------
-		
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		# 
-		self.name = name
-		if mach < 0:
-			raise IOError('Mach number should be equal or greater than zero!')
-		else:
-			self.mach = mach
-		#end
+        mach -> SCALAR: Mach Number
+        alpha -> SCALAR: Angle of Attack
+        beta -> SCALAR: Sideslip Angle
+        phat -> SCALAR: Non-Dimensional Roll Rate
+        rhat -> SCALAR: Non-Dimensional Yaw Rate
+        qhat -> SCALAR: Non-Dimensional Pitch Rate
+        
+        Attributes:
+        -----------
+        
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        # 
+        self.name = name
+        if mach < 0:
+            raise IOError('Mach number should be equal or greater than zero!')
+        else:
+            self.mach = mach
+        #end
 
-		self.alpha = alpha
-		self.beta = beta
-		self.liftIndex = liftIndex
-		self.phat = phat
-		self.rhat = rhat
-		self.qhat = qhat
-		self.degreePol = degreePol
-		self.coefPol = coefPol
-		self.degreeFourier = degreeFourier
-		self.omegaFourier = omegaFourier
-		
-		self.cosCoefFourier = cosCoefFourier
-		self.sinCoefFourier = sinCoefFourier
-		
-		self.rho = rho
-		self.P   = P
-		self.T = T
-		self.gamma = gamma
-		self.mu = mu
-		self.nu = nu
+        self.alpha = alpha
+        self.beta = beta
+        self.liftIndex = liftIndex
+        self.phat = phat
+        self.rhat = rhat
+        self.qhat = qhat
+        self.degreePol = degreePol
+        self.coefPol = coefPol
+        self.degreeFourier = degreeFourier
+        self.omegaFourier = omegaFourier
+        
+        self.cosCoefFourier = cosCoefFourier
+        self.sinCoefFourier = sinCoefFourier
+        
+        self.rho = rho
+        self.P   = P
+        self.T = T
+        self.gamma = gamma
+        self.mu = mu
+        self.nu = nu
 
-	def ListAttributes(self):
-		
-		'''
-		Print Structured Attributes List
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		ListAttributes(self)
-		
-		
-	def __str__(self):
-		
-		'''
-		Print Structured List of Variable
-		
-		Documentation last updated:  May. 21, 2008 - Ruben E. Perez
-		'''
-		
-		return ('name    mach	alpha	beta	phat	rhat	qhat\n'+'	 '+str(self.name).center(9) +'%8.2f %8.2f %8.2f %8.2f %8.2f %8.2f\n' %(self.mach, self.alpha, self.beta, self.phat, self.rhat, self.qhat))
-	
+    def ListAttributes(self):
+        
+        '''
+        Print Structured Attributes List
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        ListAttributes(self)
+        
+        
+    def __str__(self):
+        
+        '''
+        Print Structured List of Variable
+        
+        Documentation last updated:  May. 21, 2008 - Ruben E. Perez
+        '''
+        
+        return ('name    mach    alpha    beta    phat    rhat    qhat\n'+'     '+str(self.name).center(9) +'%8.2f %8.2f %8.2f %8.2f %8.2f %8.2f\n' %(self.mach, self.alpha, self.beta, self.phat, self.rhat, self.qhat))
+    
 
 
 #==============================================================================
 # 
 #==============================================================================
 def ListAttributes(self):
-		
-		'''
-		Print Structured Attributes List
-		
-		Documentation last updated:  March. 24, 2008 - Ruben E. Perez
-		'''
-		
-		print '\n'
-		print 'Attributes List of: ' + repr(self.__dict__['name']) + ' - ' + self.__class__.__name__ + ' Instance\n'
-		self_keys = self.__dict__.keys()
-		self_keys.sort()
-		for key in self_keys:
-			if key != 'name':
-				print str(key) + ' : ' + repr(self.__dict__[key])
-			#end
-		#end
-		print '\n'
-	
+        
+        '''
+        Print Structured Attributes List
+        
+        Documentation last updated:  March. 24, 2008 - Ruben E. Perez
+        '''
+        
+        print '\n'
+        print 'Attributes List of: ' + repr(self.__dict__['name']) + ' - ' + self.__class__.__name__ + ' Instance\n'
+        self_keys = self.__dict__.keys()
+        self_keys.sort()
+        for key in self_keys:
+            if key != 'name':
+                print str(key) + ' : ' + repr(self.__dict__[key])
+            #end
+        #end
+        print '\n'
+    
 
 
 #==============================================================================
 # Flow Test
 #==============================================================================
 if __name__ == '__main__':
-	
-	print 'Testing ...'
-	
-	# Test Variable
-	flow = Flow()
-	flow.ListAttributes()
-	
+    
+    print 'Testing ...'
+    
+    # Test Variable
+    flow = Flow()
+    flow.ListAttributes()
+    
