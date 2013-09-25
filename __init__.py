ba2fe872d6bbb57ba214f9d5a8026f8cfbbbd43f
Gaetan Kenway <gaetank@gmail.com>
2013-09-25 13:14:01 -0400

    modification to imports to match what mdo_import_helper expected

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import sys, os
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(cur_dir,'python'))
+
+# Modules
 import pyAS_solver
 import pyAero_flow
 import pyAero_flow_C
@@ -10,6 +12,16 @@ import pyAero_geometry
 import pyAero_solver
 import pyStruct_solver
 
+# Classes from modules
+from pyAS_solver import ASSolver
+from pyAero_flow import Flow
+from pyAero_reference import Reference 
+from pyAero_problem import AeroProblem 
+from pyStruct_solver import StructSolver
+from pyAero_geometry import Geometry
+from pyAero_solver import AeroSolver
 __all__ = ['pyAS_solver', 'pyAero_flow', 'pyAero_flow_C', 
            'pyAero_problem', 'pyAero_reference', 'pyAero_geometry',
-           'pyAero_solver','pyStruct_solver']
+           'pyAero_solver','pyStruct_solver', 
+           'ASSolver', 'Flow', 'Reference', 'StructSolver', 'Geometry', 
+           'AeroProblem', 'AeroSolver']
