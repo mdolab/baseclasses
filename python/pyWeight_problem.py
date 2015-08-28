59f75613c46fc690e4bf1eeb372f40b916929553
Charles (Sandy) Mader <charles.a.mader@gmail.com>
2015-08-27 22:31:29 -0400

    added the linear constraints to the fuel cases

--- python/pyWeight_problem.py
+++ python/pyWeight_problem.py
@@ -101,6 +101,13 @@ class WeightProblem(object):
 
         return
 
+    def _getNumComponents(self):
+        '''
+        This is a call that should only be used by MissionAnalysis
+        '''
+        return len(self.components)
+
+
     def setDesignVars(self, x):
         """
         Set the variables in the x-dict for this object.
@@ -121,6 +128,16 @@ class WeightProblem(object):
                     #print key,x[dvName]
                     xTmp = {key:x[dvName]}
                     comp.setDesignVars(xTmp)
+                    self.currentDVs[dvName]=x[dvName]
+
+        for case in self.fuelcases:
+            for key in case.DVs:
+                dvName = self.name+'_'+key
+                
+                if dvName in x:
+                    #print key,x[dvName]
+                    xTmp = {key:x[dvName]}
+                    case.setDesignVars(xTmp)
                     self.currentDVs[dvName]=x[dvName]
              
         #print 'currentDVs',self.currentDVs
@@ -144,7 +161,27 @@ class WeightProblem(object):
                     optProb.addVar(dvName, 'c', value=dv.value, lower=dv.lower,
                                    upper=dv.upper, scale=dv.scale)
 
+        for case in self.fuelcases:
+            for key in case.DVs:
+                dvName = self.name+'_'+key
+                
+                dv = case.DVs[key]
+                if dv.addToPyOpt:
+                    optProb.addVar(dvName, 'c', value=dv.value, lower=dv.lower,
+                                   upper=dv.upper, scale=dv.scale)
 
+
+    def addConstraintsPyOpt(self,optProb):
+        """
+        Add the linear constraints for each of the fuel cases.
+
+        Parameters
+        ----------
+        optProb : pyOpt_optimization class
+            Optimization problem definition to which variables are added
+        """
+        for case in self.fuelcases:
+            case.addLinearConstraint(optProb,self.name)
 
     def addFuelCases(self, cases):
         '''
@@ -163,6 +200,10 @@ class WeightProblem(object):
         for case in cases:
             self.fuelcases.append(case)
 
+            for dvName in case.DVs:
+                key = self.name+'_'+dvName
+                self.currentDVs[key] = case.DVs[dvName].value
+            # end
         return
 
     def _getComponentKeys(self, include=None, exclude=None, 
@@ -220,21 +261,74 @@ class WeightProblem(object):
 
         return weightKeys
 
-    def printComponentData(self):
+
+    def writeMassesTecplot(self,filename):
+        '''
+        Get a list of component keys based on inclusion and exclusion
+
+        Parameters
+        ----------
+        
+        filename: str
+            filename for writing the masses. This string will have the
+            .dat suffix appended to it.
+        '''
+        
+        fileHandle = filename+'.dat'
+        f = open(fileHandle,'w')
+        nMasses = len(self.nameList)
+        f.write('TITLE = "%s: Mass Data"\n'%self.name)
+        f.write('VARIABLES = "X", "Y", "Z", "Mass"\n')
+        locList = ['current','fwd','aft']
+
+        for loc in locList:
+            f.write('ZONE T="%s", I=%d, J=1, K=1, DATAPACKING=POINT\n'%(loc,nMasses))
+
+            for key in self.components.keys():
+                CG = self.components[key].getCG(loc)
+                mass =  self.components[key].getMass()
+                x= numpy.real(CG[0])
+                y= numpy.real(CG[1])
+                z= numpy.real(CG[2])
+                m= numpy.real(mass)
+
+                f.write('%f %f %f %f\n'%(x,y,z,m))
+               
+            # end
+            f.write('\n')
+        # end
+            
+        # textOffset = 0.5
+        # for loc in locList:
+        #     for name in self.nameList:
+        #         x= numpy.real(self.componentDict[name].CG[loc][0])
+        #         y= numpy.real(self.componentDict[name].CG[loc][1])
+        #         z= numpy.real(self.componentDict[name].CG[loc][2])+textOffset
+        #         m= numpy.real(self.componentDict[name].W)
+
+        #         f.write('TEXT CS=GRID3D, HU=POINT, X=%f, Y=%f, Z=%f, H=12, T="%s"\n'%(x,y,z,name+' '+loc))
+        #     # end
+            
+        # # end
+
+
+        f.close()
+        return        
+
+    def __str__(self):
         '''
         loop over the components and call the owned print function
         '''
         for key in self.components.keys():
-            self.components[key].printData()
+            print key
+            print self.components[key]
         # end
         
-        return
+        return ' '
 
 
-    def __str__(self):
-
-        self.printComponentData()
-        return 'Print statement for WeightAndBalance not implemented'
+        # self.printComponentData()
+        # return 'Print statement for WeightAndBalance not implemented'
 
 
 
@@ -263,5 +357,139 @@ class FuelCase(object):
         self.name=name
         self.fuelFraction = fuelFraction
         self.reserveFraction = reserveFraction
+
+        # Storage of DVs
+        self.DVs = {}
+        self.DVNames = {}
+        self.possibleDVs = ['fuelFraction','reserveFraction']
         
         return
+
+    def addDV(self, key, value=None, lower=None, upper=None, scale=1.0,
+              name=None, offset=0.0,axis=None, addToPyOpt=True):
+
+        """
+        Add one of the fuel case parameters as a weight and balance design
+        variable. Typical variables are fuelfraction and reservefraction.
+        An error will be given if the requested DV is not allowed to 
+        be added .
+      
+
+        Parameters
+        ----------
+        key : str
+            Name of variable to add. See above for possible ones
+
+        value : float. Default is None
+            Initial value for variable. If not given, current value
+            of the attribute will be used.
+
+        lower : float. Default is None
+            Optimization lower bound. Default is unbounded.
+
+        upper : float. Default is None
+            Optimization upper bound. Default is unbounded.
+
+        scale : float. Default is 1.0
+            Set scaling parameter for the optimization to use.
+
+        name : str. Default is None
+            Overwrite the name of this variable. This is typically
+            only used when the user wishes to have multiple
+            components explictly use the same design variable.
+
+        offset : float. Default is 0.0
+
+            Specify a specific (constant!) offset of the value used,
+            as compared to the actual design variable.
+
+        addToPyOpt : bool. Default True. 
+            Flag specifying if this variable should be added. Normally this 
+            is True. However, if there are multiple weightProblems sharing
+            the same variable, only one needs to add the variables to pyOpt
+            and the others can set this to False. 
+
+        Examples
+        --------
+        >>> # Add W variable with typical bounds
+        >>> fuelCase.addDV('fuelFraction', value=0.5, lower=0.0, upper=1.0, scale=0.1)
+        >>> fuelCase.addDV('reserveFraction', value=0.1, lower=0.0, upper=1.0, scale=0.1)
+        """
+
+        # First check if we are allowed to add the DV:
+        if key not in self.possibleDVs:
+            raise Error('The DV \'%s\' could not be added.  The list of possible DVs are: %s.'% (
+                            key, repr(self.possibleDVs)))
+
+        if name is None:
+            dvName = '%s_'% self.name + key 
+        else:
+            dvName = name
+
+        if axis is not None:
+            dvName+='_%s'%axis
+
+        if value is None:
+            value = getattr(self, key)
+
+        self.DVs[dvName] = fuelCaseDV(key, value, lower, upper, scale, offset, addToPyOpt)
+        self.DVNames[key] = dvName
+
+
+    def setDesignVars(self, x):
+        """
+        Set the variables in the x-dict for this object.
+
+        Parameters
+        ----------
+        x : dict
+            Dictionary of variables which may or may not contain the
+            design variable names this object needs
+            """
+
+        for key in self.DVNames:
+            dvName = self.DVNames[key]
+            if dvName in x:
+                setattr(self, key, x[dvName] + self.DVs[dvName].offset)
+                self.DVs[dvName].value = x[dvName]
+
+    def addLinearConstraint(self,optProb,prefix):
+        '''
+        add the linear constraint for the fuel fractions
+        '''
+        reserveDV = False
+        fuelDV = False
+        for key in self.DVNames:
+            if key.lower()=='fuelfraction':
+                fuelDV = True
+
+            if key.lower()=='reservefraction':
+                reserveDV = True
+
+        conName = prefix+'_'+self.name+'_fuelcase'
+        var1Name = prefix+'_'+self.name+'_fuelFraction'
+        var2Name = prefix+'_'+self.name+'_reserveFraction'
+        if reserveDV and fuelDV:
+            optProb.addCon(conName,lower=0,upper=1,scale=1,linear=True,wrt=[var1Name,var2Name],jac={var1Name:[[1]], var2Name:[[1]]})
+        elif reserveDV:
+            optProb.addCon(conName,lower=0,upper=1-self.fuelFraction,scale=1,linear=True,wrt=[var2Name],jac={var2Name:[[1]]})
+        elif fuelDV:
+            optProb.addCon(conName,lower=0,upper=1-self.reserveFraction,scale=1,linear=True,wrt=[var1Name],jac={var1Name:[[1]]})
+
+
+
+
+
+class fuelCaseDV(object):
+    """
+    A container storing information regarding a fuel case variable.
+    """
+    def __init__(self, key, value, lower, upper, scale, offset, addToPyOpt):
+        self.key = key
+        self.value = value
+        self.lower = lower
+        self.upper = upper
+        self.scale = scale
+        self.offset = offset    
+        self.addToPyOpt = addToPyOpt
+
