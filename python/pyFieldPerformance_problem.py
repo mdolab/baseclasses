5bd5638e65188fb8510f85b518ee3b688f01ce6e
Nick Bons <nbons@users.noreply.github.com>
2016-11-09 14:13:51 -0500

    Updated pyFieldPerformance_problem with parameter names and descriptions.

--- python/pyFieldPerformance_problem.py
+++ python/pyFieldPerformance_problem.py
@@ -5,7 +5,7 @@ pyFieldPerformance_problem
 Developers:
 -----------
 - Dr. Charles A.(Sandy) Mader (CAM)
-- Nicholas Bons   (NB)
+- Nicolas Bons   (NB)
 
 History
 -------
@@ -17,7 +17,7 @@ History
 # =============================================================================
 import numpy
 import warnings
-from ICAOAtmosphere import ICAOAtmosphere 
+from ICAOAtmosphere import ICAOAtmosphere
 
 class CaseInsensitiveDict(dict):
     def __setitem__(self, key, value):
@@ -44,15 +44,15 @@ class Error(Exception):
         msg += ' '*(78-i) + '|\n' + '+'+'-'*78+'+'+'\n'
         print(msg)
         Exception.__init__(self)
-        
+
 class FieldPerformanceProblem(object):
     """
     The main purpose of this class is to represent all relevant
     information for a single field performance analysis. This includes
-    the an internal instance of the atmospheric calculation for computing
+    an internal instance of the atmospheric calculation for computing
     density.
 
-    All parameters are optinonal except for the `name` argument which
+    All parameters are optional except for the `name` argument which
     is required. All of the parameters listed below can be acessed and
     set directly after class creation by calling::
 
@@ -62,31 +62,75 @@ class FieldPerformanceProblem(object):
     ----------
     name : str
         Name of this aerodynamic problem.
-
     funcs : iteratble object containing strings
         The names of the functions the user wants evaluated with this
         aeroProblem.
-
-    altitude : float. 
-
-    Area : float. 
-
-    Span : float. 
-
+    altitude : float
+        The altitude of the airport.
+    Area : float
+        Reference area of the wing.
+    BPR : float
+        bypass ratio of the engine
+    CDo : float
+        Zero-lift drag coefficient of airplane.
+    CDo_LG : float
+        Zero-lift drag increment due to landing gear extended.
+    CDo_HL : float
+        Zero-lift drag increment due to flap deflection.
+    CLmax : float
+        Maximum lift coefficient of the airplane in high-lift configuration.
+    runwayFrictionCoef : float
+        Friction coefficient between tires and ground, accounting for braking.
+    span : float
+        Total wingspan
+    T_I : float
+        Idle thrust for approach and landing.
+    T_G : float
+        The thrust available at 0.7*VLOF on the ground.
+    T_T : float
+        Thrust available during transition (at V = 1.15*VS).
+    T_TOS : float
+        Static thrust at takeoff (V = 0).
+    T_V2 : float
+        Thrust for safe climb with OEI (V = 1.2*VS)
+    TOGW : float
+        Takeoff gross weight
+    TSFC_I : float
+        Thrust-specific fuel consumption with engine in idle.
+    TSFC_G : float
+        Thrust-specific fuel consumption at 0.7*VLOF on the ground.
+    TSFC_T : float
+        Thrust-specific fuel consumption at V = 1.15*VS
+    WingHeight: float
+        Height of the wing above the ground.
     englishUnits : bool
-        Flag to use all English units: pounds, feet, Rankine etc. 
+        Flag to use all English units: pounds, feet, Rankine etc.
 
     Examples
     --------
-                         """
+    FP = FieldPerformance('gulfstream')
+    fpp = FieldPerformanceProblem(name='fpp1',TOGW=W,span=b,CLmax=CLmax,
+                                  WingHeight=5.6,runwayFrictionCoef=0.04,Area=S,
+                                  CDo=0.015,CDo_LG=0.0177,CDo_HL=0,
+                                  T_G=T_G,T_T=T_T,TSFC_G=TSFC,TSFC_T=TSFC,
+                                  altitude=0,englishUnits=True)
+    fpp.addDV('TOGW')
+    funcs = {}
+    funcsSens = {}
+    FP.evalFunctions(fpp, funcs, evalFuncs=['TOFL','TOFT','TOFF'])
+    FP.evalFunctionsSens(fpp, funcsSens, evalFuncs=['TOFL','TOFT','TOFF'])
+    print funcs, funcsSens
+    """
     def __init__(self, name, **kwargs):
         # Always have to have the name
         self.name = name
         # These are the parameters that can be simply set directly in
-        # the class. 
-        paras = set(( 'TOGW', 'span','CLmax','WingHeight','runwayFrictionCoef','Area',
-                      'CDo','CDo_LG','T_G','T_T','T_SLS','T_Idle','TSFC_G','TSFC_T',
-                      'TSFC_Idle', 'TSFC_SLS','altitude'))
+        # the class.
+        paras = set(('TOGW','span','WingHeight','Area',
+                    'runwayFrictionCoef','altitude',
+                    'CLmax','CDo','CDo_LG','CDo_HL',
+                    'T_G','T_T','T_V2','T_TOS','T_I',
+                    'TSFC_G','TSFC_T','TSFC_I','BPR'))
 
         # By default everything is None
         for para in paras:
@@ -96,7 +140,7 @@ class FieldPerformanceProblem(object):
         self.englishUnits = False
         if 'englishUnits' in kwargs:
             self.englishUnits = kwargs['englishUnits']
-           
+
         # create an internal instance of the atmosphere to use
         self.atm = ICAOAtmosphere(englishUnits=self.englishUnits)
 
@@ -120,23 +164,23 @@ class FieldPerformanceProblem(object):
             warnings.warn("funcs should **not** be an argument. Use 'evalFuncs'"
                           "instead.")
             self.evalFuncs = set(kwargs['funcs'])
-                
+
         # Check if 'R' is given....if not we assume air
         if 'R' in kwargs:
             self.R = kwargs['R']
         else:
             if self.englishUnits:
-                self.R = 1716.493 #FIX THIS
+                self.R = 1716.493
             else:
-                self.R = 287.870
+                self.R = 287.057
 
         # turn the kwargs into a set
         keys = set(kwargs.keys())
 
- 
         # Specify the set of possible design variables:
-        varFuncs = [ 'TOGW', 'span','Area','CDo','CDo_LG','T_G','T_T','T_SLS','T_Idle',
-                     'TSFC_G','TSFC_T','TSFC_Idle', 'TSFC_SLS']
+        varFuncs = ['TOGW','span','Area','WingHeight','CDo','CDo_LG','CDo_HL',
+                    'T_G','T_T','T_V2','T_TOS','T_I',
+                    'TSFC_G','TSFC_T','TSFC_I','BPR']
 
         self.possibleDVs = set()
         for var in varFuncs:
@@ -146,9 +190,9 @@ class FieldPerformanceProblem(object):
         # Now determine the possible functions. Any possible design
         # variable CAN also be a function (pass through)
         self.possibleFunctions = set(self.possibleDVs)
-              
+
         # When a solver calls its evalFunctions() it must write the
-        # unique name it gives to funcNames. 
+        # unique name it gives to funcNames.
         self.funcNames = {}
 
         # Storage of DVs
@@ -164,14 +208,14 @@ class FieldPerformanceProblem(object):
         P, T = self.atm(self.altitude)
         self.rho = P/(self.R*T)
 
-        
+
     def addDV(self, key, value=None, lower=None, upper=None, scale=1.0,
               name=None, dvOffset=0.0, addToPyOpt=True):
         """
         Add one of the class attributes as a design
         variable.  An error will be given if the requested DV is
-        not allowed to be added 
-      
+        not allowed to be added
+
 
         Parameters
         ----------
@@ -195,16 +239,16 @@ class FieldPerformanceProblem(object):
             Overwrite the name of this variable. This is typically
             only used when the user wishes to have multiple
             fieldPerformanceProblems explictly use the same design variable.
-            
+
         dvOffset : float. Default is 0.0
             This is the offset used to give to pyOptSparse. It can be used
-            to re-center the value about zero. 
+            to re-center the value about zero.
 
-        addToPyOpt : bool. Default True. 
-            Flag specifying if this variable should be added. Normally this 
+        addToPyOpt : bool. Default True.
+            Flag specifying if this variable should be added. Normally this
             is True. However, if there are multiple aeroProblems sharing
             the same variable, only one needs to add the variables to pyOpt
-            and the others can set this to False. 
+            and the others can set this to False.
 
         Examples
         --------
@@ -227,8 +271,8 @@ class FieldPerformanceProblem(object):
 
         if value is None:
             value = getattr(self, key)
-         
-        self.DVs[dvName] = fieldPerformanceDV(key, value, lower, upper, scale, 
+
+        self.DVs[dvName] = fieldPerformanceDV(key, value, lower, upper, scale,
                                   dvOffset, addToPyOpt)
         self.DVNames[key] = dvName
 
@@ -266,16 +310,16 @@ class FieldPerformanceProblem(object):
             dv = self.DVs[key]
             if dv.addToPyOpt:
                 optProb.addVar(key, 'c', value=dv.value, lower=dv.lower,
-                               upper=dv.upper, scale=dv.scale, 
+                               upper=dv.upper, scale=dv.scale,
                                offset=dv.dvOffset)
-            
+
     def __getitem__(self, key):
 
         return self.funcNames[key]
-        
+
     def __str__(self):
         for key,val in self.__dict__.items():
-           print ("{0:20} : {1:<16}".format(key,val))           
+           print ("{0:20} : {1:<16}".format(key,val))
 
 
     # def _getDVSens(self, func):
@@ -291,14 +335,14 @@ class FieldPerformanceProblem(object):
     #         setattr(self, key, numpy.real(getattr(self, key)))
 
     #     return rDict
-    
+
 
 class fieldPerformanceDV(object):
     """
     A container storing information regarding an 'fieldPerformance' variable.
     """
-    
-    def __init__(self, key, value, lower, upper, scale, dvOffset, 
+
+    def __init__(self, key, value, lower, upper, scale, dvOffset,
                  addToPyOpt):
         self.key = key
         self.value = value
