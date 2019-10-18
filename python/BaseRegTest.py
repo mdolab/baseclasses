87cf287f0bdb2b8a2addecefed0526a2833612f8
Eirikur Jonsson <eirikurj@umich.edu>
2019-10-17 23:22:09 -0400

    Updating the BaseRegTest class to use the builtin numpy assertion which directly supports relative and absolute tolerances. This function provides better fail messages. Optional message can be added by the user to give an idea where exactly within the test its failing

--- python/BaseRegTest.py
+++ python/BaseRegTest.py
@@ -56,77 +56,74 @@ class BaseRegTest(object):
             print(s)
 
     # Add values from root only
-    def root_add_val(self, values, rel_tol=1e-12, abs_tol=1e-12):
+    def root_add_val(self, values, rel_tol=1e-12, abs_tol=1e-12, msg=None):
         """Add values but only on the root proc"""
         if self.rank == 0:
-            self._add_values(values, rel_tol, abs_tol)
+            self._add_values(values, rel_tol, abs_tol, msg)
 
-    def root_add_dict(self, d, rel_tol=1e-12, abs_tol=1e-12):
+    def root_add_dict(self, d, rel_tol=1e-12, abs_tol=1e-12, msg=None):
         """Only write from the root proc"""
         if self.rank == 0:
-            self._add_dict(d, rel_tol, abs_tol)
+            self._add_dict(d, rel_tol, abs_tol, msg)
 
     # Add values from all processors
-    def par_add_val(self, values, rel_tol=1e-12, abs_tol=1e-12):
+    def par_add_val(self, values, rel_tol=1e-12, abs_tol=1e-12, msg=None):
         """Add value(values) from parallel process in sorted order"""
         values = self.comm.gather(values)
         if self.rank == 0:
             for i in range(len(values)):
                 print ('Value(s) on processor: %d'%i)
-                self._add_values(values[i], rel_tol, abs_tol)
+                self._add_values(values[i], rel_tol, abs_tol, msg)
 
-    def par_add_sum(self, values, rel_tol=1e-12, abs_tol=1e-12):
+    def par_add_sum(self, values, rel_tol=1e-12, abs_tol=1e-12, msg=None):
         """Add the sum of sum of the values from all processors."""
         reducedSum = self.comm.reduce(numpy.sum(values))
         if self.rank == 0:
-            self._add_value(reducedSum, rel_tol, abs_tol)
+            self._add_value(reducedSum, rel_tol, abs_tol, msg)
 
-    def par_add_norm(self, values, rel_tol=1e-12, abs_tol=1e-12):
+    def par_add_norm(self, values, rel_tol=1e-12, abs_tol=1e-12, msg=None):
         """Add the norm across values from all processors."""
         reducedSum = self.comm.reduce(numpy.sum(values**2))
         if self.rank == 0:
-            self._add_value(numpy.sqrt(reducedSum), rel_tol, abs_tol)
+            self._add_value(numpy.sqrt(reducedSum), rel_tol, abs_tol, msg)
 
     # *****************
     # Private functions
     # *****************
-    def _add_value(self, value, rel_tol=1e-12, abs_tol=1e-12):
+    def _add_value(self, value, rel_tol, abs_tol, msg):
         # We only check floats and integers
         value = numpy.atleast_1d(value).flatten()
-        assert(len(value) == 1)
+        assert(value.size == 1)
         value = value[0]
         if self.train:
             self.db.append(value)
         else:
-            self._check_value(value, self.db[self.counter], rel_tol, abs_tol)
+            self._check_value(value, self.db[self.counter], rel_tol, abs_tol, msg)
 
         self.counter += 1
 
-    def _check_value(self, val1, val2, rel_tol, abs_tol):
-        rel_err = 0
-        if val2 != 0:
-            rel_err = abs((val1-val2)/val2)
-        else:
-            rel_err = abs((val1-val2)/(val2 + 1e-16))
+    def _check_value(self, actual, reference, rel_tol, abs_tol, msg):
 
-        abs_err = abs(val1-val2)
+        if msg is None:
+            msg = "N/A"
+        msg = "Failed value for: {}".format(msg)
+        numpy.testing.assert_allclose(actual, reference, rtol=rel_tol, atol=abs_tol, err_msg=msg)
+        
 
-        assert(abs_err < abs_tol or rel_err < rel_tol)
-
-    def _add_values(self, values, rel_tol=1e-12, abs_tol=1e-12):
+    def _add_values(self, values, rel_tol, abs_tol, msg):
         '''Add values in special value format'''
         values = numpy.atleast_1d(values)
         values = values.flatten()
         for val in values:
-            self._add_value(val, rel_tol, abs_tol)
+            self._add_value(val, rel_tol, abs_tol, msg)
 
-    def _add_dict(self, d, rel_tol=1e-12, abs_tol=1e-12):
+    def _add_dict(self, d, rel_tol, abs_tol, msg):
         """Add all values in a dictionary in sorted key order"""
         for key in sorted(d.keys()):
             print ('Dictionary Key: %s'%key)
             if isinstance(d[key],dict):
                 self._add_dict(d[key], rel_tol, abs_tol)
             elif type(d[key]) == bool:
-                self._add_value(int(d[key]), rel_tol, abs_tol)
+                self._add_value(int(d[key]), rel_tol, abs_tol, msg)
             else:
-                self._add_values(d[key], rel_tol, abs_tol)
+                self._add_values(d[key], rel_tol, abs_tol, msg)
