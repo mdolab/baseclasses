598eb5f5b8084306076afa31ce87d8ee17652e0c
Josh Anibal <joanibal@umich.edu>
2020-06-16 17:03:41 -0400

    Setup (#8)
    Co-authored-by: Neil Wu <neilwu0626@gmail.com>

---
+++ baseclasses/__init__.py
@@ -0,0 +1,28 @@
+# This page is intentially left blank
+__version__ = '1.2.0'
+
+from .pyAero_problem import AeroProblem
+from .pyTransi_problem import TransiProblem
+from .pyStruct_problem import StructProblem
+from .pyAeroStruct_problem import AeroStructProblem
+
+from .pyAero_solver import AeroSolver
+from .BaseSolver import BaseSolver
+
+from .pyMission_problem import MissionProblem
+from .pyMission_problem import MissionProfile
+from .pyMission_problem import MissionSegment
+
+from .pyWeight_problem import WeightProblem
+from .pyWeight_problem import FuelCase
+
+from .FluidProperties import FluidProperties
+from .ICAOAtmosphere import ICAOAtmosphere
+from .pyEngine_problem import EngineProblem
+
+from .pyFieldPerformance_problem import FieldPerformanceProblem
+
+from .pyLG_problem import LGProblem
+
+from .py3Util import getPy3SafeString
+from .BaseRegTest import BaseRegTest
