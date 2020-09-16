06023ad6316359e47b874fe626280d5eff1b989c
Neil Wu <neilwu@umich.edu>
2020-09-16 12:11:16 -0400

    added test

---
+++ tests/test_BaseRegTest.py
@@ -0,0 +1,94 @@
+import os
+import unittest
+import copy
+import numpy as np
+from mpi4py import MPI
+from baseclasses import BaseRegTest
+from baseclasses.BaseRegTest import getTol
+
+
+comm = MPI.COMM_WORLD
+baseDir = os.path.dirname(os.path.abspath(__file__))
+root_vals = {"scalar": 1.0}
+
+root_vals_ref = copy.copy(root_vals)
+root_vals["simple dictionary"] = {"a": 1.0}
+root_vals["nested dictionary"] = {"a": {"b": 1.0, "c": 2.0}}
+root_vals_ref["simple dictionary: a"] = 1.0
+root_vals_ref["nested dictionary: a: b"] = 1.0
+root_vals_ref["nested dictionary: a: c"] = 2.0
+
+
+par_vals = {
+    "par val": [0.5, 1.5],
+    "par sum": 0.5 + 1.5,
+    "par norm": np.sqrt(2.5),
+}
+
+
+def regression_test_root(handler):
+    """
+    This function adds values from root_vals to root
+    """
+    for key, val in root_vals.items():
+        if isinstance(val, dict):
+            handler.root_add_dict(val, key)
+        elif isinstance(val, (float, int)):
+            handler.root_add_val(val, key)
+
+
+def regression_test_par(handler):
+    """
+    This function adds values in parallel
+    """
+    val = comm.rank + 0.5
+    handler.par_add_val(val, "par val")
+    handler.par_add_sum(val, "par sum")
+    handler.par_add_norm(val, "par norm")
+
+
+class TestBaseRegTest(unittest.TestCase):
+    N_PROCS = 2
+
+    def test_tol(self):
+        """
+        Test that the getTol function
+        """
+        x = 1e-1
+        y = 1e-2
+        c = 1e-3
+        d = 1e-12
+        r, a = getTol(atol=x, rtol=y)
+        self.assertEqual(a, x)
+        self.assertEqual(r, y)
+        r, a = getTol(atol=c)
+        self.assertEqual(a, c)
+        self.assertEqual(r, d)
+        r, a = getTol(rtol=c)
+        self.assertEqual(a, d)
+        self.assertEqual(r, c)
+        r, a = getTol(tol=c)
+        self.assertEqual(a, c)
+        self.assertEqual(r, c)
+
+    def test_train_then_test_root(self):
+        fileName = os.path.join(baseDir, "test_root.ref")
+        handler = BaseRegTest(fileName, train=True)
+        regression_test_root(handler)
+        handler.writeRef()
+        test_vals = handler.readRef()
+        # self.assertEqual(test_vals, root_vals_ref)
+        handler.root_print(test_vals)
+        handler.root_print(root_vals_ref)
+        handler = BaseRegTest(fileName, train=False)
+        regression_test_root(handler)
+
+    def test_train_then_test_par(self):
+        fileName = os.path.join(baseDir, "test_par.ref")
+        handler = BaseRegTest(fileName, train=True)
+        regression_test_par(handler)
+        handler.writeRef()
+        test_vals = handler.readRef()
+        self.assertEqual(test_vals, par_vals)
+        handler = BaseRegTest(fileName, train=False)
+        regression_test_par(handler)
