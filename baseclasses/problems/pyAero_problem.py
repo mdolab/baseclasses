04906227cf6be86301307ecdceeb4b8b890e1c87
Sabet Seraj <48863473+sseraj@users.noreply.github.com>
2022-03-25 14:08:10 -0400

    Clarified that AeroProblem angles are in degrees (#76)

--- baseclasses/problems/pyAero_problem.py
+++ baseclasses/problems/pyAero_problem.py
@@ -93,10 +93,10 @@ class AeroProblem(FluidProperties):
         Set the Mach number for the grid.
 
     alpha : float. Default is 0.0
-        Set the angle of attack
+        Set the angle of attack in degrees.
 
     beta : float. Default is 0.0
-        Set side-slip angle
+        Set the side-slip angle in degrees.
 
     altitude : float. Default is 0.0
         Set all thermodynamic parameters from the 1976 standard atmosphere.
