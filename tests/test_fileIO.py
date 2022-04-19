e67a6ace97a441bf87b46ebb72d9a18cb1ef5896
andv <andv@zhaw.ch>
2022-04-19 16:42:56 +0200

    Refactored to match required style

--- tests/test_fileIO.py
+++ tests/test_fileIO.py
@@ -6,7 +6,11 @@ from parameterized import parameterized
 from baseclasses.testing.decorators import require_mpi
 from baseclasses.testing.assertions import assert_equal
 from baseclasses.utils import (
-    readPickle, writePickle, readJSON, writeJSON, redirectingIO
+    readPickle,
+    writePickle,
+    readJSON,
+    writeJSON,
+    redirectingIO,
 )
 import numpy as np
 
@@ -57,17 +61,17 @@ class TestFileIO(unittest.TestCase):
         newObj = readJSON(self.fileName, comm=self.comm)
         assert_equal(obj, newObj)
 
-    @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('sys.stderr', new_callable=io.StringIO)
+    @patch("sys.stdout", new_callable=io.StringIO)
+    @patch("sys.stderr", new_callable=io.StringIO)
     def test_redirectingIO(self, mock_stdout, mock_stderr):
         mock_stdout.fileno = MagicMock(return_value=0)
         mock_stderr.fileno = MagicMock(return_value=1)
 
-        self.fileName = 'test_redirect.out'
+        self.fileName = "test_redirect.out"
 
         for i in range(2048):
-            with redirectingIO(open(self.fileName, 'a')):
-                print(f'test_{i}')
+            with redirectingIO(open(self.fileName, "a")):
+                print(f"test_{i}")
 
         # This is only here so the test is considered ok if
         # it finishes without error
