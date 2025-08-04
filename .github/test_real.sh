fb8763625e2b38f8f690099779ff5bc7d166f6a0
Alasdair Gray <alachris@umich.edu>
2025-08-04 16:24:59 -0400

    Try the same MPI env vars as pyGeo

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
+# Set OpenMPI env variables only on non-Intel MPI
+if [[ -z $I_MPI_ROOT ]]; then
+    # Set these to allow MPI oversubscription because the tests need to run on specific number of procs but the test runner may have fewer
+    export OMPI_MCA_rmaps_base_oversubscribe=1 # This works for OpenMPI <= 4
+    export PRTE_MCA_rmaps_default_mapping_policy=:oversubscribe # This works from OpenMPI >= 5
+fi
+
 cd tests
 testflo -v -n 1 --coverage --coverpkg baseclasses
